17b3de6c095ee3d3476d64315d849ebf92d70805
shaojun <wusj2017@gmail.com>
2018-08-23 15:44:32 +0800

    重新自定义view尺寸

--- app/src/main/java/com/hencoder/hencoderpracticelayout1/practice/Practice01SquareImageView.java
+++ app/src/main/java/com/hencoder/hencoderpracticelayout1/practice/Practice01SquareImageView.java
@@ -9,40 +9,40 @@ import android.widget.ImageView;
  * 需要把它写成正方形的 ImageView
  */
 public class Practice01SquareImageView extends ImageView {
-    public Practice01SquareImageView(Context context) {
-        super(context);
+  public Practice01SquareImageView(Context context) {
+    super(context);
+  }
+
+  public Practice01SquareImageView(Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public Practice01SquareImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+  /**
+   * 重写 onMeasure()，调整尺寸，让 ImageView 总是正方形
+   */
+  @Override
+  protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+    // 先用 getMeasuredWidth() 和 getMeasuredHeight() 取到 super.onMeasure() 的计算结果
+    int width = getMeasuredWidth();
+    int height = getMeasuredHeight();
+//    System.out.println("width = " + width);
+//    System.out.println("height = " + height);
+
+    // 然后通过计算，让宽度和高度一致
+    if (width >= height) {
+      width = height;
+    } else {
+      height = width;
     }
 
-    public Practice01SquareImageView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
+    // 再用 setMeasuredDimension(width, height) 来保存最终的宽度和高度
 
-    public Practice01SquareImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
-
-    /**
-     * 重写 onMeasure()，调整尺寸，让 ImageView 总是正方形
-     */
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
-        // 先用 getMeasuredWidth() 和 getMeasuredHeight() 取到 super.onMeasure() 的计算结果
-        int width = getMeasuredWidth();
-        int height = getMeasuredHeight();
-        System.out.println("width = " + width);
-        System.out.println("height = " + height);
-
-        // 然后通过计算，让宽度和高度一致
-        if (width >= height) {
-            width = height;
-        } else {
-            height = width;
-        }
-
-        // 再用 setMeasuredDimension(width, height) 来保存最终的宽度和高度
-
-        setMeasuredDimension(width, height);
-    }
+    setMeasuredDimension(width, height);
+  }
 }
--- app/src/main/java/com/hencoder/hencoderpracticelayout1/sample/Sample02CustomSizeView.java
+++ app/src/main/java/com/hencoder/hencoderpracticelayout1/sample/Sample02CustomSizeView.java
@@ -1,6 +1,10 @@
 package com.hencoder.hencoderpracticelayout1.sample;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -8,20 +12,92 @@ import android.widget.ImageView;
 
 
 public class Sample02CustomSizeView extends View {
-    public Sample02CustomSizeView(Context context) {
-        super(context);
-    }
 
-    public Sample02CustomSizeView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
+  private float mRadius = 100f;
+  private Paint mPaint = new Paint();
 
-    public Sample02CustomSizeView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    }
+  public Sample02CustomSizeView(Context context) {
+    super(context);
+  }
+
+  public Sample02CustomSizeView(Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public Sample02CustomSizeView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+  @Override
+  protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+//    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+//    int width = getWidth();
+//    int height = getHeight();
+//    System.out.println("height = " + height);
+//    System.out.println("width = " + width);
+    System.out.println("Sample02CustomSizeView.onMeasure");
+
+//    final int specMode = MeasureSpec.getMode(heightMeasureSpec);
+//    final int specSize = MeasureSpec.getSize(heightMeasureSpec);
+//
+//    System.out.println("specMode = " + specMode);
+//    System.out.println("specSize = " + specSize);
+
+    int paddingTop = getPaddingTop();
+    int paddingBottom = getPaddingBottom();
+    System.out.println("paddingTop = " + paddingTop);
+//    setMeasuredDimension(10000, 10000);
+
+    mPaint.setTextSize(20);
+    String text = "自定义View尺寸";
+    Rect bounds = new Rect();
+    mPaint.getTextBounds(text, 0, text.length(), bounds);
+    System.out.println("bounds = " + bounds);
+
+    int measuredWidth = getMeasuredWidth();
+    int measuredHeight = (int) (paddingTop + 2 * mRadius + (bounds.bottom - bounds.top) + paddingBottom);
+
+    measuredWidth = resolveSize(measuredWidth, widthMeasureSpec);
+    measuredHeight = resolveSize(measuredHeight, heightMeasureSpec);
+
+
+
+    setMeasuredDimension(measuredWidth, measuredHeight);
+
+  }
+
+  @Override
+  protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+    super.onSizeChanged(w, h, oldw, oldh);
+    System.out.println("w = [" + w + "], h = [" + h + "], oldw = [" + oldw + "], oldh = [" + oldh + "]");
+    System.out.println("Sample02CustomSizeView.onSizeChanged");
+  }
+
+  @Override
+  protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
+    System.out.println("Sample02CustomSizeView.onDraw");
+
+
+    int measuredWidth = getMeasuredWidth();
+    int measuredHeight = getMeasuredHeight();
+
+    System.out.println("measuredWidth = " + measuredWidth);
+    System.out.println("measuredHeight = " + measuredHeight);
+
+    mPaint.setColor(Color.BLUE);
+
+
+    canvas.drawCircle(0, 0, 100, mPaint);
+
+  }
+
+  @Override
+  protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+    super.onLayout(changed, left, top, right, bottom);
+    System.out.println("Sample02CustomSizeView.onLayout");
+
+  }
 }
